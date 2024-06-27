6b06fdb37b665a5b5b0d5471a3827edfbfc2ac9f
Mohammad Amin <ghorbannezhadm2@gmail.com>
2024-06-27 20:08:20 +0330

    adding the unit test file for the second project

---
+++ secondProject/game_test.py
@@ -0,0 +1,10 @@
+'''
+this file should contain test cases for the worlde game
+the test cases should contain different words array, and
+we should mimic action user for guessing the word in different
+approaches and the expected response should be asserted equalled to
+the application actual response.
+
+for the inspiration see the test file in firstProject Directory
+'''
+
